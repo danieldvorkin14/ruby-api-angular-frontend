40d23330aef83422ccd174e0f8a6ea53c9f9e3db
Daniel Dvorkin <daniel.dvorkin@teknion.com>
2020-04-02 15:15:43 -0400

    Did some more work, got all the pages built and connected

--- frontend/src/app/components/customer-add/customer-add.component.ts
+++ frontend/src/app/components/customer-add/customer-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/customer';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-add',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add.component.scss']
 })
 export class CustomerAddComponent implements OnInit {
+  customer: Customer = new Customer();
 
-  constructor() { }
+  constructor(public apiService: ApiService, public acRoute: ActivatedRoute, public router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.acRoute.params.subscribe((data: any) => {
+      if(data && data.id) {
+        this.apiService.get("/customers/" + data.id).subscribe((data: Customer) => {
+          this.customer = data;
+        });
+      } else {
+        this.customer = new Customer();
+      }
+    });
   }
 
+  onSubmit() {
+    if(this.customer.id){
+      this.apiService.update("customers/" + this.customer.id, this.customer).subscribe((r) => {
+        this.router.navigateByUrl("/customers");
+      });
+    } else {
+      this.apiService.post("customers", this.customer).subscribe((r) => {
+        this.customer = new Customer();
+
+        this.router.navigateByUrl("/customers");
+      });
+    }
+  }
 }
